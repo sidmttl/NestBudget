526b792866e7a7d317975a31f994e520ef702b81
manchi-s <manchi.s@northeastern.edu>
2025-03-30 21:35:22 -0400

    adding signup activity and layout

---
+++ app/src/main/java/com/example/nestbudget/User.java
@@ -0,0 +1,49 @@
+package com.example.nestbudget;
+
+public class User {
+    public String firstName;
+    public String lastName;
+    public String username;
+    public String password;
+    public String age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
